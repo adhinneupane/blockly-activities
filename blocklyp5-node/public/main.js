89a32948aed1ad5952b1e362cce45594d65d93c3
neupaneadhin <86452437+neupaneadhin@users.noreply.github.com>
2022-03-31 18:02:10 -0500

    added execution logic

--- blocklyp5-node/public/main.js
+++ blocklyp5-node/public/main.js
@@ -1,8 +1,25 @@
 var workspace = Blockly.inject('toolboxDiv', {toolbox: toolbox});
 
-function makecode(){
+function makeCode(){
 var code = Blockly.JavaScript.workspaceToCode(workspace);
 console.log("i am here:", code);
-document.getElementById("output").innerHTML=code;
-
+document.getElementById("printscreen").innerHTML=code;
 }
+
+
+
+function runCode(){
+    const script = document.createElement('script');
+    var executable = document.getElementById('printscreen').innerHTML;
+    script.innerText= executable;
+    console.log(executable);
+    script.setAttribute('async','true');
+    script.onload = function handleScriptLoad(){
+        console.log('The p5 script has loaded')      
+    }    
+    script.onerror = function handleScriptError(){
+        console.log('An error has occured')
+    }
+    document.head.appendChild(script); 
+}
+
