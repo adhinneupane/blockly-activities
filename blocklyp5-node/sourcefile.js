6225209fe63444d6405b4d5afbd773e62e5f89f7
neupaneadhin <86452437+neupaneadhin@users.noreply.github.com>
2022-04-27 19:00:12 -0500

    save backup

--- blocklyp5-node/sourcefile.js
+++ blocklyp5-node/sourcefile.js
@@ -1,13 +1,13 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(900, 900);
   noLoop();
   textSize(20); 
 } 
 
 function draw() {
  background(220);
- columnNames = ["serial No","caterpillar","leaves","leaves1","leaves2"];
- userEntries = [99,98,94,93];
+ columnNames = ["serial No","caterpillar","leaves","leaves1","leaves2","leaves3","leaves4"];
+ userEntries = [10,20,30,40,50,60];
  sumEnabled = true;
  showEnabled = true;
  rowstoCopy = 0;
@@ -19,8 +19,11 @@ function draw() {
  table = new p5.Table();
  rowCount = 0;
  copiedRow = 0;
-let rowsToAdd = [];
+ rowsToAdd = [1,2,3,4];
 let copyValues = [];
+  if (sumEnabled == true){
+    totalCount = 1; 
+  }
 
  for(let i=0;i<columnNames.length;i++){
      table.addColumn(columnNames[i]);
@@ -44,7 +47,7 @@ let copyValues = [];
      for (let c = 0; c < 1; c++) {
      text(table.columns, 20 + 80 * c, 80); 
      } 
-     for (let r = 0; r < rowCount + countCopies; r++){ 
+     for (let r = 0; r < rowCount + countCopies + totalCount; r++){ 
      for (let c = 0; c < columnNames.length; c++) {
               text(table.getString(r, c), 20 + 80 * c, 100 + 20 * r);
           }
@@ -69,15 +72,30 @@ copyRow('0');
 copyRow('1');
 copyRow('2');
   
-function Total(rowsToAdd){
-let sum = [];
-let placeholder = 0;
-for (let i = 1; i <table.getColumnCount(); i++){
- for (let j=0; j<table.getRowCount(); j++){
-   sum[i] = 0;
-   placeholder = '' 
- }
-}
+function Total(){
+let sum = []
+let totalRow = table.addRow();
+totalRow.setString(columnNames[0],"Sum");
+k = 0;
+placeholder = 0;
+  // totalRow.setString(table.getRowCount()-1,0, ""+ "Sum");
+
+for (let j = 0 ; j < table.getColumnCount()-1; j++)  {
+  for (let i = 0; i < rowsToAdd.length; i++ ){
+      sum [k] = parseInt(table.getString(rowsToAdd[i]-1,j+1));  
+      k = k + 1;  
+      placeholder += sum[i];
+  }
+  print(sum);
+  print (placeholder);
+  totalRow.setString(columnNames[j+1],placeholder);
+  }
+  print ("rows", table.getRowCount())
+  print ("columns",table.getColumnCount())
+  for (let j = 0 ; j <= table.getColumnCount()-1; j++)  {
+    print(table.getString(table.getRowCount()-1, j));
+  text(table.getString(table.getRowCount()-1, j), 20 + 80 * 7, 100 + 20 * 7);
+  }
 }
 
 Total();
