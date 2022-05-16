77d131ff767e2110e7755fb9413bf7db73b72482
neupaneadhin <86452437+neupaneadhin@users.noreply.github.com>
2022-05-16 16:02:32 -0500

    node-server

---
+++ blocklyp5-abstract/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+const path = require('path');
+
+const router = express.Router;
+
+var app = express();
+var PORT = 8080;
+
+app.use(express.static('public'))
+app.use('/home', function(request, response){  
+    response.sendFile(__dirname + '/example.html');
+});
+
+ 
+app.post("/run", (request, response) => {
+    response.send("posted");
+    console.log("recieved");
+   });
+
+
+ 
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
