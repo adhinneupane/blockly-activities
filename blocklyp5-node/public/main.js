c2cc3f006986977a713fd96b5d8f3e9935e91bde
ashutoshneupane <adhinneupane2020@gmail.com>
2022-08-29 14:03:00 -0500

    save code

--- blocklyp5-node/public/main.js
+++ blocklyp5-node/public/main.js
@@ -1,7 +1,32 @@
 // insert blockly to front end
+var blocklyArea = document.getElementById('blocklyArea');
+var blocklyDiv = document.getElementById('blocklyDiv');
+var workspace = Blockly.inject(blocklyDiv,
+    {toolbox: document.getElementById('toolbox')});
+var onresize = function(e) {
+  // Compute the absolute coordinates and dimensions of blocklyArea.
+  var element = blocklyArea;
+  var x = 0;
+  var y = 0;
+  do {
+    x += element.offsetLeft;
+    y += element.offsetTop;
+    element = element.offsetParent;
+  } while (element);
+  // Position blocklyDiv over blocklyArea.
+  blocklyDiv.style.left = x + 'px';
+  blocklyDiv.style.top = y + 'px';
+  blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+  blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+  Blockly.svgResize(workspace);
+};
+window.addEventListener('resize', onresize, false);
+onresize();
+Blockly.svgResize(workspace);
+
 
 var workspace = Blockly.inject('toolboxDiv', {toolbox: toolbox});
-var span = document.getElementsByClassName("close")[0];
+
 let executableString = ''
 
 // used to refresh the canvas on each run
